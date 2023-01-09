900e2c08e8f2294d643a3a1364d55233d50d641e
Loli <lolarlew@gmail.com>
2023-01-09 17:21:09 -0300

    saludo usuario

---
+++ main.js
@@ -0,0 +1,63 @@
+// usuario
+class Usuario {
+  constructor(usuario) {
+    this.usuario = usuario;
+  }
+}
+
+const arrayUsuarios = [];
+
+const formUsuario = document.getElementById("formUsuario");
+
+
+formUsuario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  // crear usuario nuevo
+  const nombre = document.getElementById("nombre");
+  const usuario = new Usuario(nombre.value);
+  arrayUsuarios.push(usuario);
+
+  // verifico x consola
+  console.log(arrayUsuarios);
+
+  // reset form
+  formUsuario.reset();
+
+  // saludar usuario
+  const contenedorSaludo = document.getElementById("contenedorSaludo");
+  const saludo = document.createElement("p");
+  saludo.innerText = `Hola ${usuario.value}`;
+  contenedorSaludo.appendChild(saludo);
+})
+
+
+// Buscador
+const buscador = document.getElementById("buscador");
+
+buscador.addEventListener("submit", (e) => {
+  // Evito que vuelva a cargar la página
+  e.preventDefault();
+  buscador.reset();
+})
+
+// Constructor de productos
+class Producto {
+  constructor (nombre, precio, id) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.id = id
+  }
+}
+
+// Productos
+const kanpaiPandas = new Producto ("Kanpai Pandas", 8.4, 1);
+const cryptoPunks = new Producto ("Crypto Punks", 7.6, 2);
+const boredApe = new Producto ("Bored Ape Yacht Club", 5.9, 3);
+const wonderPals = new Producto ("Wonder Pals", 3.2, 4);
+
+// Array de productos
+const articulos = [kanpaiPandas, cryptoPunks, boredApe, wonderPals];
+
+// Array de productos vacía en la cual se iran almacenando los precios de los productos seleccionados
+const carrito = [];
+
